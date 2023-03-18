af16bc656f9a708f81809af74c79b545f328755c
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-03-18 15:32:18 +0530

    feat: add logging with morgan

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import { createStream, RotatingFileStream } from 'rotating-file-stream';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
+// create a rotating write stream
+const accessLogStream : RotatingFileStream = createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(process.cwd(), 'log')
+})
+
+app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(express.static("public"));
+
 app.get('/ping', async (_req: Request, res: Response) => {
     res.send({
-        message: "pong"
+        message: "pong ping"
     });
 });
 
